c4ab428d0ddce78d01a0ec61458016487d2dc3fa
ning <885154692@qq.com>
2023-11-27 21:15:30 +0800

    fix: bug

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -33,7 +33,7 @@ function Login() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h3 className="text-2xl font-semibold leading-none tracking-tight">Sign UP</h3>
+      <h3 className="text-2xl font-semibold leading-none tracking-tight">Login</h3>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
         <div>
           <label htmlFor="username" className="inline-block w-24">
