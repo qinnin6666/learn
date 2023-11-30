530af1170298ef57b576b685a096eab97ae57c2b
ning <885154692@qq.com>
2023-11-30 23:38:42 +0800

    add: user

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,14 @@
+'use client'
 import Link from 'next/link'
 import React, { FC } from 'react'
 import Logout from './Logout'
+import { trpc } from '@/app/_trpc/client'
 
 interface NavBarProps {}
 
 const NavBar: FC<NavBarProps> = ({}) => {
+  const { data: user } = trpc.user.me.useQuery()
+
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <div className="max-w-5xl mx-auto relative h-full">
@@ -21,8 +25,13 @@ const NavBar: FC<NavBarProps> = ({}) => {
           <Link href="/post/post-one">post</Link>
         </div>
         <div className="absolute right-0 top-0 h-full flex justify-end items-center gap-1">
-          <LinkButton href="/login" />
-          <LinkButton href="/signup" />
+          {user ? <div>{user.name}</div> : (
+            <>
+              <LinkButton href="/login" />
+              <LinkButton href="/signup" />
+            </>
+          )}
+
           <Logout />
         </div>
       </div>
@@ -34,7 +43,8 @@ function LinkButton({ href }: { href: string }) {
   return (
     <Link
       href={href}
-      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm transition-colors h-10 px-2 py-1 bg-slate-600 hover:bg-slate-800 text-white">
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm transition-colors h-10 px-2 py-1 bg-slate-600 hover:bg-slate-800 text-white"
+    >
       {href.slice(1)}
     </Link>
   )
